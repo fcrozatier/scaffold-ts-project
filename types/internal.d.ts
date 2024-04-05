5462eb6f3ce540c7d6d5edcaccaf510c3787c099
fcrozatier <frederic.crozatier@protonmail.com>
2024-04-05 17:31:49 +0200

    add internal types

--- types/internal.d.ts
+++ types/internal.d.ts
@@ -4,29 +4,3 @@ export type Options = {
 	eslint: boolean;
 	vitest: boolean;
 };
-
-export type File = {
-	name: string;
-	contents: string;
-};
-
-export type Condition =
-	| "eslint"
-	| "prettier"
-	| "typescript"
-	| "checkjs"
-	| "playwright"
-	| "vitest"
-	| "skeleton"
-	| "default"
-	| "skeletonlib"
-	| "svelte5";
-
-export type Common = {
-	files: Array<{
-		name: string;
-		include: Condition[];
-		exclude: Condition[];
-		contents: string;
-	}>;
-};
